a26303e761ad7904f22c4f3cbe18dfb7900bf5b2
dominickasanga <dominickasanga@gmail.com>
2024-02-06 10:27:17 +0200

    modified:   sidekiq.rb

--- sidekiq.rb
+++ sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-    config.redis = { url: 'redis://192.168.1.137:6379/0' }
+    config.redis = { url: 'redis://0.0.0.0:6379/0' }
 end
   
 Sidekiq.configure_client do |config|
-    config.redis = { url: 'redis://192.168.1.137:6379/0' }
+    config.redis = { url: 'redis://0.0.0.0:6379/0' }
 end
